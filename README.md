436a26471702394190886b5ca3b1b437c530c411
unknown <wuzhifeng@zhairport.net>
2019-11-05 13:19:44 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Java使用kafka实现发布与订阅
+
+
+### 1.安装kafka
+````
+docker pull wurstmeister/zookeeper
+docker pull wurstmeister/kafka
+````
+````
+docker run -d --name zookeeper -p 2181:2181 -t wurstmeister/zookeeper
+docker run -d --name kafka --publish 9092:9092 --link zookeeper --env KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 --env KAFKA_ADVERTISED_HOST_NAME=192.168.4.111 --env KAFKA_ADVERTISED_PORT=9092 wurstmeister/kafka:latest
+````
+
+### 2.编译
+````
+cd java-kafka
+mvn install
+````
+
+### 3.启动订阅程序
+````
+mvn exec:java -Dexec.mainClass="com.frontng.demo.kafka.Consumer"
+````
+
+### 4.启动发布程序
+````
+mvn exec:java -Dexec.mainClass="com.frontng.demo.kafka.Producer"
+````
---
+++ src/main/java/com/frontng/demo/kafka/Producer.java
@@ -0,0 +1,42 @@
+package com.frontng.demo.kafka;
+
+import java.util.Properties;
+import java.util.Random;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+/**
+ * 生产者端
+ */
+public class Producer {
+
+    /**
+     * 定义主题
+     */
+    public static String TOPIC = "frontng_test";
+
+    public static void main(String[] args) throws InterruptedException {
+        Properties p = new Properties();
+        //kafka地址，多个地址用逗号分割
+        p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.4.111:9092");
+        p.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        p.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(p);
+
+        try {
+            while (true) {
+                String msg = "Hello," + new Random().nextInt(100);
+                ProducerRecord<String, String> record = new ProducerRecord<String, String>(TOPIC, msg);
+                kafkaProducer.send(record);
+                System.out.println("消息发送成功:" + msg);
+                Thread.sleep(500);
+            }
+        } finally {
+            kafkaProducer.close();
+        }
+
+    }
+}
